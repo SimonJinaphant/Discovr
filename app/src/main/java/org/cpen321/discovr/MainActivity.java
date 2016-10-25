d4f6abaaf6dd8d1315138e245363ef25976011bd
Valerian Ratu <Valerian Ratu>
2016-10-24 17:59:32 -0700

    Basic fragment UI framework put in place

--- app/src/main/java/org/cpen321/discovr/MainActivity.java
+++ app/src/main/java/org/cpen321/discovr/MainActivity.java
@@ -1,121 +1,76 @@
 package org.cpen321.discovr;
+import android.os.Bundle;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
+
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v4.app.ActivityCompat;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.mapbox.mapboxsdk.MapboxAccountManager;
-import com.mapbox.mapboxsdk.annotations.MarkerViewOptions;
-import com.mapbox.mapboxsdk.camera.CameraPosition;
-import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
-import com.mapbox.mapboxsdk.exceptions.InvalidAccessTokenException;
-import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
-import com.mapbox.mapboxsdk.maps.MapboxMap;
-import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
-    private MapView mapView;
-    private static final int REQUEST_ALL_MAPBOX_PERMISSIONS = 3211;
+    NavigationView navigationView = null;
+    Toolbar toolbar = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // TODO: Refactor permissions code
-        String[] permissions = {
-                Manifest.permission.ACCESS_FINE_LOCATION,
-        };
-
-        for (int i = 0; i < permissions.length; i++) {
-            int hasFineLocation = ActivityCompat.checkSelfPermission(this, permissions[i]);
-            if (hasFineLocation != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(this, new String[]{permissions[i]}, REQUEST_ALL_MAPBOX_PERMISSIONS);
-            }
-        }
-
-        try {
-            // Only this method throws an exception for invalid token
-            MapboxAccountManager.validateAccessToken(getString(R.string.mapbox_key));
-
-            // Must present API key BEFORE calling setContentView() on any view containing MapView
-            // This should save you 3+ hours of debugging why your valid API key isn't working...
-            MapboxAccountManager.start(this, getString(R.string.mapbox_key));
-
-        } catch (InvalidAccessTokenException e) {
-            System.err.println("Invalid access token: " + e);
-        }
-
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        //Set the fragment initially
+        MapViewFragment fragment = new MapViewFragment();
+        android.support.v4.app.FragmentTransaction fragmentTransaction =
+                getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.commit();
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Are you hungry?", Snackbar.LENGTH_LONG)
-                        .setAction("Find me food!", new View.OnClickListener() {
-                            @Override
-                            public void onClick(View v) {
-                                mapView.getMapAsync(new OnMapReadyCallback() {
-                                    @Override
-                                    public void onMapReady(MapboxMap mapboxMap) {
-                                        MarkerViewOptions timHortons = new MarkerViewOptions()
-                                                .position(new LatLng(49.260131, -123.248534))
-                                                .title("Forestry Tim Hortons")
-                                                .snippet("Where the line-up never gets short :(");
-
-                                        CameraPosition cameraPosition = new CameraPosition.Builder()
-                                                .target(timHortons.getPosition())
-                                                .bearing(270)
-                                                .tilt(50)
-                                                .zoom(17)
-                                                .build();
-
-                                        mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition), 5000, null);
-                                        mapboxMap.addMarker(timHortons);
-                                    }
-
-                                });
-                            }
-                        }).show();
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
 
-        mapView = (MapView) findViewById(R.id.mapView);
-        mapView.onCreate(savedInstanceState);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.app_name, R.string.app_name);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
 
-        // Load up zoom-in animation for the central UBC Vancouver campus
-        mapView.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(MapboxMap mapboxMap) {
-                CameraPosition cameraPosition = new CameraPosition.Builder()
-                        .target(mapboxMap.getCameraPosition().target)
-                        .bearing(mapboxMap.getCameraPosition().bearing)
-                        .tilt(50)
-                        .zoom(16)
-                        .build();
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
 
-                mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition), 5000, null);
-            }
-
-        });
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.drawer_view, menu);
         return true;
     }
 
@@ -134,33 +89,36 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public void onResume() {
-        super.onResume();
-        mapView.onResume();
-    }
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
 
-    @Override
-    public void onPause() {
-        super.onPause();
-        mapView.onPause();
-    }
+        if (id == R.id.map_view) {
+            MapViewFragment fragment = new MapViewFragment();
+            android.support.v4.app.FragmentTransaction fragmentTransaction =
+                    getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, fragment);
+            fragmentTransaction.commit();
+        } else if (id == R.id.events_subscribed) {
 
-    @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-        mapView.onLowMemory();
-    }
+            EventsSubscribedFragment fragment = new EventsSubscribedFragment();
+            android.support.v4.app.FragmentTransaction fragmentTransaction =
+                    getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, fragment);
+            fragmentTransaction.commit();
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        mapView.onDestroy();
-    }
+        } else if (id == R.id.events_nearby) {
 
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        mapView.onSaveInstanceState(outState);
+        } else if (id == R.id.events_all) {
+
+        } else if (id == R.id.events_create) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
