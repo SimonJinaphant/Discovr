2217d98410236d3a608b4a4e13463656a4bf067b
jtkchoi <jacquelinetkchoi@gmail.com>
2016-11-06 12:33:13 -0800

    Add Locally generated Events
    * SQLite initial setup, database handler, eventInfo class
    
    * Updates to EventInfo class
    
    * start dynamically creating buttons in EventsSubscribedFragment
    
    * individual tables for each field in event type
    
    * eventInfo data type changes & preliminary db testing
    
    * db getEventBySearch method implemented
    
    * Debugged getEventBySearch
    
    * changed xml for events_subscribed to make scrollable & reformat text when dynamically displaying buttons
    
    * start creating on click listener for each event - will open new fragment w/ event details and map in background
    
    * start dynamically creating buttons in EventsSubscribedFragment
    
    * individual tables for each field in event type
    
    * eventInfo data type changes & preliminary db testing
    
    * db getEventBySearch method implemented
    
    * Debugged getEventBySearch
    
    * changed xml for events_subscribed to make scrollable & reformat text when dynamically displaying buttons
    
    * start creating on click listener for each event - will open new fragment w/ event details and map in background

--- app/src/main/java/org/cpen321/discovr/EventsSubscribedFragment.java
+++ app/src/main/java/org/cpen321/discovr/EventsSubscribedFragment.java
@@ -1,18 +1,35 @@
 package org.cpen321.discovr;
 
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
+import android.text.SpannableString;
+import android.text.style.AbsoluteSizeSpan;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+
+import java.util.List;
+import java.util.ListIterator;
+
+import static android.text.Spanned.SPAN_INCLUSIVE_INCLUSIVE;
+import static org.cpen321.discovr.R.dimen.button_margin;
+import static org.cpen321.discovr.R.id.left;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class EventsSubscribedFragment extends Fragment {
-
 
     public EventsSubscribedFragment() {
         // Required empty public constructor
@@ -22,8 +39,55 @@ public class EventsSubscribedFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        //Get DBHandler for this activity
+        SQLiteDBHandler dbh = new SQLiteDBHandler(this.getActivity());
+
+        //Get List of all events stored in DB
+        List<EventInfo> allEvents = dbh.getAllEvents();
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_events_subscribed, container, false);
+        FrameLayout fm = (FrameLayout) inflater.inflate(R.layout.fragment_events_subscribed, container, false);
+        ScrollView sv = (ScrollView) fm.getChildAt(0);
+        //Get linearlayour and layoutParams for new button
+        LinearLayout ll = (LinearLayout) sv.getChildAt(0);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        //Add new button for each event in DB
+        for(EventInfo event : allEvents) {
+            Button button = new Button(getActivity());
+
+            //Set ID of button = id of event
+            button.setId(event.getID());
+
+            //Set button properties - gravity, allCaps, padding, backgroundColor, textColor, text
+            button.setGravity(left);
+            button.setAllCaps(false);
+            button.setPadding(getResources().getDimensionPixelSize(button_margin), getResources().getDimensionPixelSize(button_margin), getResources().getDimensionPixelSize(button_margin), getResources().getDimensionPixelSize(button_margin));
+            button.setBackground(ContextCompat.getDrawable(getContext(),R.drawable.button_press_colors));
+            button.setTextColor(ContextCompat.getColor(getContext(), R.color.primaryTextColor));
+            SpannableString buttonText = new SpannableString(event.getName() + "\n" + event.getStartTime() + " - " + event.getEndTime() + ", in " + event.getLocation());
+            int index = buttonText.toString().indexOf("\n");
+            buttonText.setSpan(new AbsoluteSizeSpan(60), 0, index, SPAN_INCLUSIVE_INCLUSIVE);
+            buttonText.setSpan(new AbsoluteSizeSpan(40), index, buttonText.length(), SPAN_INCLUSIVE_INCLUSIVE);
+            button.setText(buttonText);
+
+            //Add arrow to end of button
+            Drawable arrow = ContextCompat.getDrawable(getContext(), R.drawable.right_arrow);
+            button.setCompoundDrawablesRelativeWithIntrinsicBounds(null, null, arrow, null);
+            button.setOnClickListener(new View.OnClickListener() {
+                                          @Override
+                                          public void onClick(View v) {
+                                              Log.d("Button has been pressed", "Perform some action");
+                                          }
+                                      });
+
+
+            //Add new button to linearlayout with all properties set above and layour params
+            ll.addView(button, lp);
+        }
+
+        return fm;
     }
 
 }
---
+++ app/src/main/java/org/cpen321/discovr/SQLiteDBHandler.java
@@ -0,0 +1,183 @@
+package org.cpen321.discovr;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by jacqueline on 10/28/2016.
+ */
+
+public class SQLiteDBHandler extends SQLiteOpenHelper{
+    //Database version
+    private static final int DATABASE_VERSION = 1;
+    //Database name
+    private static final String DATABASE_NAME = "LocalEvents";
+    //Table name
+    private static final String TABLE_SUBSCRIBED_EVENTS = "SubscribedEvents";
+    //Column names in table "SubscribedEvents"
+    private static final String KEY_ID = "id";
+    private static final String KEY_NAME = "name";
+    private static final String KEY_HOST = "hostName";
+    private static final String KEY_BUILDING = "buildingName";
+    private static final String KEY_STARTTIME = "startTime";
+    private static final String KEY_ENDTIME = "endTime";
+    private static final String KEY_LOCATION = "location";
+    private static final String KEY_DETAILS = "eventDetails";
+
+    public SQLiteDBHandler (Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    //Creates table when getReadableDatabase or getWritableDatabase is called and no DB exists
+    @Override
+    public void onCreate(SQLiteDatabase db){
+        String CREATE_TABLE = "CREATE TABLE " + TABLE_SUBSCRIBED_EVENTS + "(" +
+                KEY_ID + " INTEGER PRIMARY KEY," +
+                KEY_NAME + " TEXT," +
+                KEY_HOST + " TEXT," +
+                KEY_BUILDING + " TEXT," +
+                KEY_STARTTIME + " TEXT," +
+                KEY_ENDTIME + " TEXT," +
+                KEY_LOCATION + " TEXT," +
+                KEY_DETAILS + " TEXT" + ");";
+        db.execSQL(CREATE_TABLE);
+    }
+
+    //Creates new db if new version > old version
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+        //Drop older tables if they existed
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_SUBSCRIBED_EVENTS);
+        //Recreate table
+        onCreate(db);
+    }
+
+    //Adds new event to database
+    public void addEvent(EventInfo data){
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        //Place values in contentValues
+        ContentValues values = new ContentValues();
+        values.put(KEY_ID, data.getID());
+        values.put(KEY_NAME, data.getName());
+        values.put(KEY_HOST, data.getHostName());
+        values.put(KEY_BUILDING, data.getBuildingName());
+        values.put(KEY_STARTTIME, data.getStartTime());
+        values.put(KEY_ENDTIME, data.getEndTime());
+        values.put(KEY_LOCATION, data.getLocation());
+        values.put(KEY_DETAILS, data.getEventDetails());
+
+        //Insert new row
+        db.insert(TABLE_SUBSCRIBED_EVENTS, null, values);
+
+        //close database connection
+        db.close();
+    }
+
+    //Get one specific event based on ID
+    public EventInfo getEvent(int id){
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        //Select all rows from TABLE_SUBSCRIBED_EVENTS where key_ID = id
+        Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_SUBSCRIBED_EVENTS + " WHERE " + KEY_ID + " = ? ", new String[]{String.valueOf(id)} );
+        if (cursor != null)
+        cursor.moveToFirst();
+
+        EventInfo event = new EventInfo(cursor.getInt(0), cursor.getString(1), cursor.getString(2), cursor.getString(3), cursor.getString(4), cursor.getString(5), cursor.getString(6), cursor.getString(7));
+        return event;
+    }
+
+    public List<EventInfo> getEventbySearch(String searchString){
+        List<EventInfo> eventList = new ArrayList<EventInfo>();
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        //Select all rows from TABLE_SUBSCRIBED_EVENTS where KEY_NAME contains searchString or KEY_BUILDING contains searchString or KEY_HOST contains searchString
+        String query = "SELECT * FROM " + TABLE_SUBSCRIBED_EVENTS + " WHERE " + KEY_NAME + " LIKE ? OR " + KEY_BUILDING + " LIKE ? OR " + KEY_HOST + " LIKE ?";
+
+        Cursor cursor = db.rawQuery(query, new String[]{"%" + searchString + "%", "%" + searchString + "%", "%" + searchString + "%"});
+
+        //Add all those elements to a list
+        if (cursor.moveToFirst()) {
+            do {
+                Log.d("found events", cursor.getString(1));
+                EventInfo event = new EventInfo(cursor.getInt(0), cursor.getString(1), cursor.getString(2), cursor.getString(3), cursor.getString(4), cursor.getString(5), cursor.getString(6), cursor.getString(7));
+                eventList.add(event);
+            }
+            while (cursor.moveToNext());
+
+        }
+
+        //close cursor and return list of all events
+        cursor.close();
+        return eventList;
+    }
+
+    //Get all events in user's database
+    public List<EventInfo> getAllEvents() {
+        List<EventInfo> eventList = new ArrayList<EventInfo>();
+
+        //Select all rows from TABLE_SUBSCRIBED_EVENTS
+        String selectQuery = "SELECT * FROM " + TABLE_SUBSCRIBED_EVENTS;
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        //loop through each row and add that event to the list
+        if (cursor.moveToFirst()) {
+            do {
+                EventInfo event = new EventInfo(cursor.getInt(0), cursor.getString(1), cursor.getString(2), cursor.getString(3), cursor.getString(4), cursor.getString(5), cursor.getString(6), cursor.getString(7));
+                eventList.add(event);
+            }
+            while (cursor.moveToNext());
+        }
+
+        //close cursor and return list of all events
+        cursor.close();
+        return eventList;
+    }
+
+    //Get count of all events in user's database
+    public int getEventCount(){
+        String query = "SELECT * FROM " + TABLE_SUBSCRIBED_EVENTS;
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.rawQuery(query, null);
+        cursor.close();
+
+        return cursor.getCount();
+    }
+
+    //Updates single Event
+    public int updateEvent(EventInfo event){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        values.put(KEY_ID, event.getID());
+        values.put(KEY_NAME, event.getName());
+        values.put(KEY_BUILDING, event.getBuildingName());
+        values.put(KEY_HOST, event.getHostName());
+        values.put(KEY_STARTTIME, event.getStartTime());
+        values.put(KEY_ENDTIME, event.getEndTime());
+        values.put(KEY_LOCATION, event.getLocation());
+        values.put(KEY_DETAILS, event.getEventDetails());
+
+        //update that row
+        return db.update(TABLE_SUBSCRIBED_EVENTS, values, KEY_ID + " = ?", new String[]{String.valueOf(event.getID())});
+    }
+
+    //Delete single Event
+    public void deleteEvent(EventInfo event){
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(TABLE_SUBSCRIBED_EVENTS, KEY_ID + " = ?", new String[]{String.valueOf(event.getID())});
+        db.close();
+    }
+
+}
+
