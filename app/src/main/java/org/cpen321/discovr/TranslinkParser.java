f4110d46a45a95b5f2eb8825701672f7fa08c2df
SansNickel <nleung.nl@gmail.com>
2016-11-05 19:24:11 -0700

    function takes in stop number and outputs list<string>

--- app/src/main/java/org/cpen321/discovr/TranslinkParser.java
+++ app/src/main/java/org/cpen321/discovr/TranslinkParser.java
@@ -3,7 +3,8 @@ package org.cpen321.discovr;
 
 import java.io.IOException;
 import java.net.URL;
-
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -15,36 +16,31 @@ import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
 public class TranslinkParser {
-	static void printproperties() throws IOException, ParserConfigurationException {
-		
-	
+	static List<String> nextBuses(int stopnum) throws IOException, ParserConfigurationException {
 		try {
-			String url = "http://api.translink.ca/rttiapi/v1/stops/59275/estimates?apikey=VxujSiOu28llUoMXPgmw";
+			String url = "http://api.translink.ca/rttiapi/v1/stops/" + stopnum + "/estimates?apikey=VxujSiOu28llUoMXPgmw";
+					
 			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 			DocumentBuilder db = dbf.newDocumentBuilder();
 			Document doc = db.parse(new URL(url).openStream());
-
 			
+		    List<String> result = new ArrayList<String>();
+
 			for (int i = 0; i < doc.getElementsByTagName("RouteNo").getLength(); i++) {
-				System.out.println(doc.getElementsByTagName("RouteNo").item(i).getTextContent());
+				result.add(doc.getElementsByTagName("RouteNo").item(i).getTextContent());
 				for (int j = 0; j < doc.getElementsByTagName("Schedules").item(i).getChildNodes().getLength(); j++)	{
-					System.out.println(doc.getElementsByTagName("Schedules").item(i).getChildNodes().item(j).getFirstChild().getNextSibling().getNextSibling().getTextContent());
+					result.add(doc.getElementsByTagName("Schedules").item(i).getChildNodes().item(j).getFirstChild().getNextSibling().getNextSibling().getTextContent());
 				}
 			}
-			/*
-		    	for (int i = 0; i < 10; i++) {
-					
-					System.out.print(doc.getElementsByTagName("ExpectedLeaveTime").item(i).getTextContent());
-					System.out.println(" in " + doc.getElementsByTagName("ExpectedCountdown").item(i).getTextContent() + " minutes");
-				}
-			*/
-			
+			return result;
 		} catch (SAXException exception) {
-			System.out.print(exception);
-			throw new IOException("rip");
+			throw new IOException("error parsing");
+		} catch (IOException exception) {
+			throw new IOException("invalid bus number");
 		}
 	}
 	public static void main(String[] args) throws IOException, ParserConfigurationException {
-		printproperties();
+		for (String i : nextBuses(59275))
+			System.out.println(i);
 	}
 }
