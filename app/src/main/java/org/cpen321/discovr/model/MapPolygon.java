095d6944990529b61e5cdc8e25f44cf160b9cf86
SimonJinaphant <SimonJinaphant@users.noreply.github.com>
2016-11-08 16:26:12 -0800

    Add model for bus stops and modified comments for mappolygon

--- app/src/main/java/org/cpen321/discovr/model/MapPolygon.java
+++ app/src/main/java/org/cpen321/discovr/model/MapPolygon.java
@@ -5,11 +5,15 @@ import com.mapbox.mapboxsdk.geometry.LatLng;
 import java.util.List;
 
 /**
- * Model class of a drawable polygon on the MapboxMap
+ * Model class of a drawable polygon on the MapboxMap.
+ * A polygon is meant to mark a general area that's not confined to one building.
  */
 
-public class MapPolygon {
+public class MapPolygon{
+    // The name of the polygon area.
     public final String name;
+
+    // Collection of Latitude/Longitude which forms the polygon vertices.
     public final List<LatLng> vertices;
 
     public MapPolygon(String name, List<LatLng> vertices){
