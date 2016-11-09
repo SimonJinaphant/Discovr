6dc15a838361b51189a1b366c1821d452c5d105b
Jacqueline Choi <Jacqueline Choi>
2016-11-09 14:49:43 -0800

    started building information in db

---
+++ app/src/main/java/org/cpen321/discovr/BuildingInformation.java
@@ -0,0 +1,39 @@
+package org.cpen321.discovr;
+
+/**
+ * Created by jacqu on 11/9/2016.
+ */
+
+public class BuildingInformation {
+    private String name;
+    private String code;
+    private String address;
+    private String hours;
+    private String coordinates;
+
+    BuildingInformation(String name, String code, String address, String hours, String coordinates){
+        this.name = name;
+        this.code = code;
+        this.address = address;
+        this.hours = hours;
+        this.coordinates = coordinates;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getCode(){
+        return this.code;
+    }
+    public String getAddress(){
+        return this.address;
+    }
+    public String getHours(){
+        return this.hours;
+    }
+
+    public String getCoordinates(){
+        return this.coordinates;
+    }
+}
