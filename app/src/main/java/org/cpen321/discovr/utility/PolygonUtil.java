22afae8eb2ae6d8e77647b9dd2dedb4ead1d7aee
Simon Jinaphant <SimonJinaphant@users.noreply.github.com>
2016-11-09 19:00:28 -0800

    Add demo-able polygon intersection (#84)
    * Add PolygonUtil and simple unit test for it
    
    * Add Valerian's getLatLng onclick code
    
    * Somewhat working point in polygon detection
    
    * Working with construction data
    
    * Stop after finding first match
    
    * Demo-able point in polygon system

---
+++ app/src/main/java/org/cpen321/discovr/utility/PolygonUtil.java
@@ -0,0 +1,133 @@
+package org.cpen321.discovr.utility;
+
+import com.mapbox.mapboxsdk.geometry.LatLng;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+
+/**
+ * Created by Simon Jinaphant on 09-Nov-2016.
+ */
+
+public class PolygonUtil {
+
+    private static boolean isZero(double value, double threshold){
+        return value >= -threshold && value <= threshold;
+    }
+
+    private static int orientation(LatLng p1, LatLng p2, LatLng q1){
+        double val = (p2.getLongitude() - p1.getLongitude()) * (q1.getLatitude() - p2.getLatitude())
+                - (q1.getLongitude() - p2.getLongitude()) * (p2.getLatitude() - p1.getLatitude());
+        if (isZero(val, 0.0000001))
+            return 0;
+        else
+            return (val < 0) ? -1 : 1;
+    }
+
+    private static boolean onSegment(LatLng p1, LatLng p2, LatLng q) {
+        if (min(p1.getLatitude(), p2.getLatitude()) <= q.getLatitude()
+                && q.getLatitude() <= max(p1.getLatitude(), p2.getLatitude())
+                && min(p1.getLongitude(), p2.getLongitude()) <= q.getLongitude()
+                && q.getLongitude() <= max(p1.getLongitude(), p2.getLongitude()))
+            return true;
+        else
+            return false;
+    }
+
+    private static boolean intersectionTest(LatLng p1, LatLng p2, LatLng p3, LatLng p4) {
+        int o1 = orientation(p1, p2, p3);
+        int o2 = orientation(p1, p2, p4);
+        int o3 = orientation(p3, p4, p1);
+        int o4 = orientation(p3, p4, p2);
+
+        // General case
+        if (o1 != o2 && o3 != o4)
+            return true;
+
+        // Special cases
+        if (o1 == 0 && onSegment(p1, p2, p3))
+            return true;
+        if (o2 == 0 && onSegment(p1, p2, p4))
+            return true;
+        if (o3 == 0 && onSegment(p3, p4, p1))
+            return true;
+        if (o4 == 0 && onSegment(p3, p4, p2))
+            return true;
+
+        return false;
+    }
+
+    public static boolean pointInPolygon(LatLng p, LatLng[] polygon) {
+
+        if (polygon.length < 3) {
+            return false; // Flawed polygon
+        }
+        LatLng PtoInfinity = new LatLng(Long.MAX_VALUE, p.getLongitude());
+
+        int intersectionsCount = 0;
+        int i = 0, j = i + 1;
+        // Same Y coordinate points have to be counted once
+        Set<LatLng> sameYcoordPoints = new HashSet<>();
+        do {
+
+            if (intersectionTest(p, PtoInfinity, polygon[i], polygon[j]) == true) {
+
+                boolean invalidIntersection = false;
+                if (p.getLongitude() == polygon[i].getLongitude() || p.getLongitude() == polygon[j].getLongitude()) {
+
+                    boolean res = sameYcoordPoints.contains(polygon[i]);
+                    // Possible collision
+                    if (res) {
+                        invalidIntersection = true;
+                    }
+                    res = sameYcoordPoints.contains(polygon[j]);
+                    // Possible collision
+                    if (res) {
+                        invalidIntersection = true;
+                    }
+                    if (p.getLongitude() == polygon[i].getLongitude())
+                        sameYcoordPoints.add(polygon[i]);
+                    else if (p.getLongitude() == polygon[j].getLongitude())
+                        sameYcoordPoints.add(polygon[j]);
+                }
+
+                if (!invalidIntersection) {
+
+                    ++intersectionsCount;
+
+                    if (orientation(polygon[i], polygon[j], p) == 0) { // Collinear
+                        if (onSegment(polygon[i], polygon[j], p) == true)
+                            return true;
+                        else {
+                            // Exception case when point is collinear but not on segment
+                            // e.g.
+                            //           *  ************
+                            //             /            \
+                            //            k              w
+                            // The collinear segment is worth 0 if k and w have the same
+                            // vertical direction
+
+                            int k = (((i - 1) >= 0) ? // Negative wraparound
+                                    (i - 1) % (polygon.length) : (polygon.length) + (i - 1));
+                            int w = ((j + 1) % polygon.length);
+
+                            if ((polygon[k].getLongitude() <= polygon[i].getLongitude() && polygon[w].getLongitude() <= polygon[j].getLongitude())
+                                    || (polygon[k].getLongitude() >= polygon[i].getLongitude() && polygon[w].getLongitude() >= polygon[j].getLongitude()))
+                                --intersectionsCount;
+                        }
+                    }
+                }
+            }
+
+            i = ((i + 1) % polygon.length);
+            j = ((j + 1) % polygon.length);
+
+        } while (i != 0);
+
+        return (intersectionsCount % 2 != 0);
+    }
+
+}
---
+++ app/src/test/java/org/cpen321/discovr/PolygonUtilTest.java
@@ -0,0 +1,46 @@
+package org.cpen321.discovr;
+
+import com.mapbox.mapboxsdk.geometry.LatLng;
+
+import org.cpen321.discovr.utility.PolygonUtil;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Simon Jinaphant on 09-Nov-2016.
+ */
+
+public class PolygonUtilTest {
+
+    LatLng[] polygon = {
+            new LatLng(49.269207, -123.252354),
+            new LatLng(49.269062, -123.252226),
+            new LatLng(49.26898, -123.252461),
+            new LatLng(49.268822, -123.252332),
+            new LatLng(49.268812, -123.252358),
+            new LatLng(49.268834, -123.252381),
+            new LatLng(49.26882, -123.252431),
+            new LatLng(49.268951, -123.252543),
+            new LatLng(49.268899, -123.252685),
+            new LatLng(49.269045, -123.252807)
+    };
+
+    @Test
+    public void inPolygon() throws Exception {
+        LatLng p1 = new LatLng(49.269066, -123.252498);
+        assertEquals(true, PolygonUtil.pointInPolygon(p1, polygon));
+
+        LatLng p2 = new LatLng(49.269171, -123.252367);
+        assertEquals(true, PolygonUtil.pointInPolygon(p2, polygon));
+    }
+
+    @Test
+    public void notInPolygon(){
+        LatLng p1 = new LatLng(49.266229, -123.252933);
+        assertEquals(false, PolygonUtil.pointInPolygon(p1, polygon));
+
+        LatLng p2 = new LatLng(49.266901, -123.252707);
+        assertEquals(false, PolygonUtil.pointInPolygon(p2, polygon));
+    }
+}
