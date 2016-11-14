447096553bdea7a9e84f296cae563d6e957f440e
ValRat <leri.ratu@gmail.com>
2016-11-13 22:30:11 -0800

    Framework for fragment click testing

--- app/src/androidTest/java/org/cpen321/discovr/MainActivityTest.java
+++ app/src/androidTest/java/org/cpen321/discovr/MainActivityTest.java
@@ -1,18 +1,25 @@
 package org.cpen321.discovr;
 
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.ViewInteraction;
 import android.support.test.espresso.action.ViewActions;
 import android.support.test.espresso.contrib.DrawerActions;
+import android.support.test.espresso.matcher.BoundedMatcher;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.v7.widget.Toolbar;
 import android.test.ActivityInstrumentationTestCase2;
 import android.view.KeyEvent;
 import android.widget.EditText;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.closeSoftKeyboard;
+import static android.support.test.espresso.Espresso.getIdlingResources;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -26,8 +33,11 @@ import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.isFocusable;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withResourceName;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.allOf;
 
 /**
  * Created by valerian on 11/9/16.
@@ -49,13 +59,56 @@ public class MainActivityTest{
 
     //TODO: Create test opening all fragment elements
 
+
+    // Title checking code credit: http://blog.sqisland.com/2015/05/espresso-match-toolbar-title.html
+    private static ViewInteraction matchToolbarTitle(
+            CharSequence title) {
+        return onView(isAssignableFrom(Toolbar.class))
+                .check(matches(withToolbarTitle(is(title))));
+    }
+
+    private static Matcher<Object> withToolbarTitle(
+            final Matcher<CharSequence> textMatcher) {
+        return new BoundedMatcher<Object, Toolbar>(Toolbar.class) {
+            @Override public boolean matchesSafely(Toolbar toolbar) {
+                return textMatcher.matches(toolbar.getTitle());
+            }
+            @Override public void describeTo(Description description) {
+                description.appendText("with toolbar title: ");
+                textMatcher.describeTo(description);
+            }
+        };
+    }
+
+
     // 1. Open up Events Nearby
+    @Test
+    public void testEventsNearby(){
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+        onView(withText(R.string.events_nearby)).perform(click());
+        //Check whether the right title shows up
+        CharSequence title = InstrumentationRegistry.getTargetContext().getString(R.string.events_nearby);
+        matchToolbarTitle(title);
+        //TODO: Check whether the proper actions occur on the map
+    }
 
     // 2. Open up Subscribed Events
+    @Test
+    public void testSubscribedEvents(){
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+        onView(withText(R.string.events_subscribed)).perform(click());
+        //Check whether the right title shows up
+        CharSequence title = InstrumentationRegistry.getTargetContext().getString(R.string.events_subscribed);
+        matchToolbarTitle(title);
+        //TODO: Check for fragment existence
+    }
+
 
     // 3. Open up all events
 
     // 4. Open up course list
+
+
 
     // TODO: Testing fragment switching
     // 5. Open up Subscribed Events Fragment, then open up view map fragment
